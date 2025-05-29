eb7291d1603d69b2cbe66eab16ec8b24c0cf64b0
Russell L Bates <russell.l.bates@gmail.com>
2025-05-28 23:34:44 -0500

    Fix ForEach id syntax

--- CardGame/PartyStatusView.swift
+++ CardGame/PartyStatusView.swift
@@ -18,7 +18,7 @@ struct PartyStatusView: View {
                         Text("Stress \(character.stress)/9")
                             .font(.caption2)
                         HStack(spacing: 2) {
-                            ForEach(1...9, id: .self) { index in
+                            ForEach(1...9, id: \.self) { index in
                                 Image(character.stress >= index ? "icon_stress_pip_lit" : "icon_stress_pip_unlit")
                                     .resizable()
                                     .frame(width: 12, height: 12)
@@ -30,17 +30,17 @@ struct PartyStatusView: View {
                         Text("Harm")
                             .font(.caption2)
                         HStack {
-                            ForEach(0..<HarmState.lesserSlots, id: .self) { index in
+                            ForEach(0..<HarmState.lesserSlots, id: \.self) { index in
                                 Image(index < character.harm.lesser.count ? "icon_harm_lesser_full" : "icon_harm_lesser_empty")
                                     .resizable()
                                     .frame(width: 16, height: 16)
                             }
-                            ForEach(0..<HarmState.moderateSlots, id: .self) { index in
+                            ForEach(0..<HarmState.moderateSlots, id: \.self) { index in
                                 Image(index < character.harm.moderate.count ? "icon_harm_moderate_full" : "icon_harm_moderate_empty")
                                     .resizable()
                                     .frame(width: 16, height: 16)
                             }
-                            ForEach(0..<HarmState.severeSlots, id: .self) { index in
+                            ForEach(0..<HarmState.severeSlots, id: \.self) { index in
                                 Image(index < character.harm.severe.count ? "icon_harm_severe_full" : "icon_harm_severe_empty")
                                     .resizable()
                                     .frame(width: 16, height: 16)
